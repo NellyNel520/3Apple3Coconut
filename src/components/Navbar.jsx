f2838e62577fdb3daa4581744f990ea97878869e
NellyNel520 <channelharris520@gmail.com>
2023-12-04 00:50:32 -0500

    completed navbar will add google font later

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Logo2 from '../assets/fullLogo-foodTruck-gray.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="shadow-md w-full sticky top-0 left-0">
+      <div className="md:flex items-center justify-between  text-red-600 py-4 px-2">
+        <div 	className="font-bold text-2xl cursor-pointer flex items-center font-ari">
+        <span className="w-[6.5rem] mr-2">
+						<img
+							src={Logo2}
+							alt="logo"
+						/>
+					</span>
+						<span className='mt-12'>3 Apple 3 Coconut</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
