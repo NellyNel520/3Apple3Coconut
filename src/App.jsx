8227798aceaa00f88c98f0b87b51797c12e07465
NellyNel520 <channelharris520@gmail.com>
2023-12-03 23:19:00 -0500

    added browser routing to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import UnderConstruction from './pages/UnderConstruction'
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <div className='text-blue-500'>hello</div>
-    </>
-  )
+	return (
+		<div className="App">
+			<Navbar />
+			<main>
+				<Routes>
+					<Route path="/" element={<UnderConstruction />} />
+				</Routes>
+			</main>
+		</div>
+	)
 }
 
 export default App
