14f242b7e3769410d43a7327a8b3a3cb11e7dc2a
NellyNel520 <channelharris520@gmail.com>
2024-02-13 18:28:31 -0500

    created files for new pages as well as routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import UnderConstruction from './pages/UnderConstruction'
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import Catering from './pages/Catering'
+import AboutUs from './pages/AboutUs'
+import Gallery from './pages/Gallery'
+import Schedule from './pages/Schedule'
+import Specials from './pages/Specials'
+import ContactUs from './pages/ContactUs'
 import './index.css'
 
 function App() {
@@ -8,6 +16,14 @@ function App() {
 			<main>
 				<Routes>
 					<Route path="/" element={<UnderConstruction />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/menu" element={<Menu />} />
+					<Route path="/catering" element={<Catering />} />
+					<Route path="/about-us" element={<AboutUs />} />
+					<Route path="/gallery" element={<Gallery />} />
+					<Route path="/schedule" element={<Schedule />} />
+					<Route path="/specials" element={<Specials />} />
+					<Route path="/contact" element={<ContactUs />} />
 				</Routes>
 			</main>
 		</div>
